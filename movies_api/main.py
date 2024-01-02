d0ee0b2eb7f3a7f06b552757f50aa5d65126df42
Peter Marklund <peter@marklunds.com>
2024-01-02 13:50:29 +0100

    test db connection to Cloud SQL with SQLAlchemy

--- movies_api/main.py
+++ movies_api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from movies_api.database import connect_with_connector
 
 app = FastAPI(title="Movies API")
+
+engine = connect_with_connector()
+conn = engine.connect()
+print(conn)
 
 @app.get("/health", response_class=JSONResponse)
 def health():
